dd966789135e93517b15a20c76e288ce9161d504
Jessie <jessie.griffin@icloud.com>
2020-05-24 10:32:24 -0400

    Adds function to calculate area and perimeter and set text fields to results

--- RectangleCalculatorApp/ViewController.swift
+++ RectangleCalculatorApp/ViewController.swift
@@ -17,9 +17,17 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
+    func calculateAreaAndPerimeter() {
+        guard let width = widthTextField.text, let height = heightTextField.text else { return } // notify the user to enter a value
+        guard let widthDouble = Double(width), let heightDouble = Double(height) else { return } // notify the user to enter a number
 
+        let area = widthDouble * heightDouble
+        let perimeter = (widthDouble * 2) + (heightDouble * 2)
+        areaLabel.text = "\(area)"
+        perimeterLabel.text = "\(perimeter)"
+
+    }
 }
 
